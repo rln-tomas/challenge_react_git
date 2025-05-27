cb87ad459d6d0992b9fcd7736ca11a3a74728e1e
Tomás Rolón <tomas.ezequiel.rln@gmail.com>
2025-05-27 13:44:46 -0300

    Change layout  & remove default index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
-import { Stack } from "expo-router";
+import { createStackNavigator } from "@react-navigation/stack";
+import { PaperProvider } from "react-native-paper";
+import { FavoritesProvider } from "../context/FavoritesContext";
+import { RootStackParamList } from "./navigation/types";
+import HomeScreen from "./screens/Home/HomeScreen";
+import UserDetailScreen from "./screens/UserDetail/UserDetailScreen";
+
+const StackNav = createStackNavigator<RootStackParamList>();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <PaperProvider>
+      <FavoritesProvider>
+        <StackNav.Navigator>
+          <StackNav.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <StackNav.Screen
+            name="UserDetail"
+            component={UserDetailScreen}
+            options={{
+              title: "Detalle del usuario",
+              headerBackTitle: "Atrás",
+            }}
+          />
+        </StackNav.Navigator>
+      </FavoritesProvider>
+    </PaperProvider>
+  );
 }
